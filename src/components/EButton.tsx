954513faa7ae3530362a13ca0edc76bfc0ccc041
LilyP87 <apavilla@outlook.fr>
2023-06-27 11:50:57 +0200

    add ternary

--- src/components/EButton.tsx
+++ src/components/EButton.tsx
@@ -1,7 +1,7 @@
 import { EButtonProps } from "../interfaces/interface_App";
 
 export const EButton = ({ children, onClick, classArray }: EButtonProps) => {
-    const classStr = classArray.join(' ')
+    const classStr = classArray ? classArray.join(' ') : " "
     return (
         <>
             <button className={classStr + " ebutton"} onClick={onClick}>
