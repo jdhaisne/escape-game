7d36512f89a4da26d2a54d927e8dd2c6e9e7f762
jdhaisne <julien.dhaisne@gmail.com>
2023-06-27 10:56:51 +0200

    added EModal

--- src/components/EHeader.tsx
+++ src/components/EHeader.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import { EModal } from "./EModal";
+
 export const EHeader = () => {
+  const [isShowing, setIsShowing] = useState<boolean>(false);
+  const onClick = () => {
+    setIsShowing(!isShowing);
+  };
   return (
     <>
       <div className="header">
         <div className="header__logo">logo</div>
-        <div className="header__button">login/logout</div>
+        <div className="header__button" onClick={onClick}>
+          login/logout
+        </div>
       </div>
+      <EModal isShowing={isShowing} setIsShowing={setIsShowing}>
+        <button>bg</button>
+      </EModal>
     </>
   );
 };
