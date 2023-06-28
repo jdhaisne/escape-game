eb0553350a21fc84efc61fc2e334ab263ca01194
lily <apavilla@outlook.fr>
2023-06-28 23:45:55 +0200

    Stylisation du footer

--- src/components/EFooter-component.tsx
+++ src/components/EFooter-component.tsx
@@ -1,8 +1,21 @@
 export const EFooter = () => {
   return (
     <div className="footer">
-      <div className="footer__section">1</div>
-      <div className="footer__section">2</div>
+      <div className="footer__section">
+        <p>17, place de la Nation </p>
+        <p>75011 PARIS </p>
+        <p>+33 12 34 56 78 90</p>
+      </div>
+      <div className="footer__section footer-middle">
+    <p>Mentions légales</p>
+    <p>Conditions Générales d'Utilisation</p>
+    <p>Contact</p>
+      </div>
+      <div className="footer__section footer-social-media">
+        <div className="facebook"></div>
+        <div className="instagram"></div>
+        <div className="twitter"></div>
+      </div>
     </div>
   );
 };
