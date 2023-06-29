a7a58cdb93db92cd7eaa1dabbdcf8f44c190098f
jdhaisne <julien.dhaisne@gmail.com>
2023-06-29 16:45:30 +0200

    started room

---
+++ src/components/ERoom.tsx
@@ -0,0 +1,39 @@
+//image, titre, desc, age
+
+export const ERoom = ({
+  imgPath,
+  title,
+  desc,
+  age,
+}: {
+  imgPath: string;
+  title: string;
+  desc: string;
+  age: string;
+}) => {
+  return (
+    <div className="room">
+      <img
+        className="room__img"
+        src={imgPath}
+        width="300px
+      "
+        height="200px"
+      />
+      <div className="room__title__wrapper">
+        <h2 className="room__title">{title}</h2>
+      </div>
+      <div className="room__desc">{desc}</div>
+      <div className="room__footer">
+        <span
+          className="room__link
+"
+        >
+          detail
+        </span>
+        <span className="room__link">reserver</span>
+        <img className="room_pegi" />
+      </div>
+    </div>
+  );
+};
--- src/pages/EHome-page.tsx
+++ src/pages/EHome-page.tsx
@@ -1,28 +1,45 @@
-import { useEffect } from "react"
-import { getUsers } from "../services/ESUser-api"
+import { useEffect } from "react";
+import { getUsers } from "../services/ESUser-api";
 import { logger } from "../services/ESLogger";
+import { ERoom } from "../components/ERoom";
 
-
-
-export const EHomePage : React.FunctionComponent = () =>
-{
-  useEffect(() => 
-  {
+export const EHomePage: React.FunctionComponent = () => {
+  useEffect(() => {
     getUsers()
-      .then(users => {
-        logger.info(users)
-        logger.debug(users)
-        logger.warning(users)
-        logger.error(users)
+      .then((users) => {
+        logger.info(users);
+        logger.debug(users);
+        logger.warning(users);
+        logger.error(users);
       })
-      .catch(error => {
-        console.error('Error retrieving users:', error);
+      .catch((error) => {
+        console.error("Error retrieving users:", error);
       });
-  }, [])
+  }, []);
 
-return (
+  return (
     <>
       <h1>PAGE FROM HOME !</h1>
+      <div>
+        <ERoom
+          title="test"
+          desc="test test"
+          imgPath="https://lockacademy.com/wp-content/uploads/2020/07/un-crime-presque-parfait-escape-game-lock-academy.jpg"
+          age="10"
+        ></ERoom>
+        <ERoom
+          title="test"
+          desc="test test"
+          imgPath="https://lockacademy.com/wp-content/uploads/2020/07/un-crime-presque-parfait-escape-game-lock-academy.jpg"
+          age="10"
+        ></ERoom>
+        <ERoom
+          title="test"
+          desc="test test"
+          imgPath="https://lockacademy.com/wp-content/uploads/2020/07/un-crime-presque-parfait-escape-game-lock-academy.jpg"
+          age="10"
+        ></ERoom>
+      </div>
     </>
-    )
-}
+  );
+};
