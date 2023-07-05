341dfd3fb3195a7eaeb852f81cc88ca778bd82b1
jdhaisne <julien.dhaisne@gmail.com>
2023-07-05 17:05:17 +0200

    changed navigate

--- src/components/header/EHeader-component.tsx
+++ src/components/header/EHeader-component.tsx
@@ -1,8 +1,7 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { SUser } from "../../services/ESUser";
 
-import './style.scss'
-
+import "./style.scss";
 
 export const EHeader = () => {
   const isLoggedIn = SUser.isConnected();
@@ -24,14 +23,16 @@ export const EHeader = () => {
           <div className="header__logo"></div>
         </NavLink>
 
-        <div className="header__wrapper"> 
-          {isLoggedIn && (<NavLink
-            to={`history/${SUser.getId()}`}
-            className={"header__button header__history"}
-          >
-            History
-          </NavLink>)}
-          
+        <div className="header__wrapper">
+          {isLoggedIn && (
+            <NavLink
+              to={`/history/${SUser.getId()}`}
+              className={"header__button header__history"}
+            >
+              History
+            </NavLink>
+          )}
+
           <NavLink
             to={isLoggedIn ? "/" : "/login"}
             className={"header__button " + (isLoggedIn ? "logout" : "login")}
